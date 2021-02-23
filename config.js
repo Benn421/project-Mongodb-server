eae3446c05312061c79a8e100701ba0e60bf2446
Ben T <55330892+Benn421@users.noreply.github.com>
2021-02-23 15:10:03 -0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    MONGODB: 
-        'mongodb+srv://Ben421:fBUtY3afeyrXYtV@cluster0.2cdlw.mongodb.net/database?retryWrites=true&w=majority',
-    SECRET_KEY:
-        'some very secret key'
-};
